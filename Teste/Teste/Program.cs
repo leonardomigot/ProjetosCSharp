e6ccda46ebba71393901b956781ba11d05945a05
Leonardo Migot <leonardomigot@hotmail.com>
2021-11-11 13:41:24 -0300

    alt

--- Teste/Teste/Program.cs
+++ Teste/Teste/Program.cs
@@ -1,17 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Teste {
     class Program {
         static void Main( string[] args ) {
-            GravarDados();
+            //GravarDados();
+            RecuperarProdutos();
+        }
+
+        private static void RecuperarProdutos()
+        {
+            using (var repo = new LojaContext())
+            {
+                IList<Produto> produtos = repo.Produtos.ToList();
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine(item.Nome);
+                }
+            }
         }
 
         private static void GravarDados() {
             Produto prod = new Produto();
 
-            prod.Nome = "jovem";
-            prod.Categoria = "cliente";
-            prod.Preco = 78.98;
+            prod.Nome = "jovemd";
+            prod.Categoria = "cliented";
+            prod.Preco = 738.98;
 
             using(var contexto = new LojaContext()) {
                 contexto.Produtos.Add( prod );
