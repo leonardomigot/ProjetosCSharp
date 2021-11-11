c628a9cf9825d8593ec79aa77b94a87ee71284a7
Leonardo Migot <leonardomigot@hotmail.com>
2021-11-11 10:02:38 -0300

    Subindo

---
+++ Teste/Teste/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste {
+    public class Produto {
+
+        public int Id { get; protected set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public double Preco { get; set; }
+    }
+}
---
+++ Teste/Teste/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Teste {
+    class Program {
+        static void Main( string[] args ) {
+            GravarDados();
+        }
+
+        private static void GravarDados() {
+            Produto prod = new Produto();
+
+            prod.Nome = "jovem";
+            prod.Categoria = "cliente";
+            prod.Preco = 78.98;
+
+            using(var contexto = new LojaContext()) {
+                contexto.Produtos.Add( prod );
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
